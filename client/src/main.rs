0859de3ab81d0bf78013abe6f25674f8096f914b
TechNinja14 <techninja14@github.com>
2024-04-23 01:41:37 -0500

    ready for deployment

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,37 +1,40 @@
 #![allow(unused_must_use)]
 #![allow(dead_code)]
 #![allow(unused_variables)]
+#![allow(unreachable_code)]
 extern crate reqwest;
-//use std::process::Command;
+use std::process::Command;
 use std::collections::HashMap;
+use std::time::Duration;
+use async_std::task;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    
-    let map = HashMap::from([("user", "abel")]);
-    #[derive(serde::Deserialize)]
-    #[derive(Debug)]
-    struct Data {
-        title: String,
-        description: String,
-        date: String,
-        time: String,
-        address: String
-    }
+    loop { 
+        let map = HashMap::from([("user", "abel")]);
+        let client = reqwest::Client::new();
+        let request = client.get("http://localhost:8080/notification").json(&map)
+            .send()
+            .await
+            .unwrap()
+            .text()
+            .await
+            .unwrap();
 
-    let client = reqwest::Client::new();
-    let request = client.get("http://localhost:8080/test").json(&map)
-        .send()
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap();
-//    let mut command = Command::new("dunstify");
-    let parsed = json::parse(&request).unwrap();
-//    command.arg("hello").spawn();
-    println!("{:?}", parsed);
-    println!("{:?}", parsed[0]["title"]);
-    assert_eq!(parsed[0]["title"],"test title");
+        let parsed = json::parse(&request).unwrap();
+        let notif = parsed["notification"].to_string();
+
+        if notif == "true" {
+
+            let data = &parsed["data"];
+            let parms = Vec::from([format!("Reminder for {}", data["time"]), data["desc"].to_string(), "-u".to_string(), "critical".to_string()]);
+            let command = Command::new("dunstify").
+                args(&parms).spawn();
+
+        } else {
+            //do nothing
+        }
+        task::sleep(Duration::from_secs(30)).await;
+    }
     Ok(())
 }
--- server/index.js
+++ server/index.js
@@ -6,44 +6,77 @@ let static_dir = path.join(__dirname, 'static');
 
 let app = express();
 app.use(express.static(static_dir));
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.set("view engine", "ejs");
 
+let db = [];
 
-let db = [
-	{ 	title: 'test title',
-		description: 'do you work',
-		date: '2024-04-20',
-		time: '00:07',
-		address: '127.0.0.1:8080'
-	}
-];
-
-app.get('/test', (req, res) => {
-	
-	if (req.body.user === "abel") {
-		console.log(req.body, "connected!");
-		
-		res.json(db);
-	
-	} else {
-		res.json(db);
-	}
-
-});
-
-app.get('/time', (req, res) => {
+function rightTime(entry) {
 
 	let time = new Date();
+	time.setHours(time.getHours() - 5);
 	var [date, hour] = time.toISOString().split('T');
 	hour = hour.substring(0, 5);
 	console.log(date, hour);
 
-	if (date === db[0].date && hour === db[0].time) {
-		console.log("correct time!!");
-		res.send({notification: true, db});
+	if (date === entry.date && hour === entry.time) {
+		return true;
 	} else {
-		res.send({notification: false});
+		return false;
 	}
-})
+
+};
+
+app.get('/', (req, res) => {
+	res.redirect('/form');
+
+});
+
+app.get('/form', (req, res) => {
+	res.render('form', {status: "none"});
+});
+
+//TODO: fix time to match region
+//and delete db entry once it is sent
+app.post('/form', (req, res) => {
+	const sh = req.body;
+	if (	sh.user.length == 0 ||
+		sh.time.length == 0 ||
+		sh.date.length == 0 ||
+		sh.desc.length == 0 ||
+		sh.title.length == 0) 
+	{
+		console.log("Error: missing data");
+		res.render('form', {status: "error"});
+	} else {
+		console.log("Success: entry added");
+		res.render('form', {status: "new"});
+		db.push(JSON.parse(JSON.stringify(req.body)));
+	}
+});
+app.get('/notification', (req, res) => {
+	
+	let should_remove = null;
+
+	for (let x of db) {
+		//if notif is found matching username, return it and exit
+		if (x.user === req.body.user) {
+			if (rightTime(x)) {
+				console.log(req.body.user,`connected!\nsending messages...`);
+				should_remove = x.user;
+				res.json({notification: true, data: x});
+			}
+		} 
+	}
+	if (should_remove != null) {
+		db = db.filter(e => { return e.user != should_remove });
+		return;
+	}
+	//only runs if no user was found
+	res.json({notification: false});
+
+
+});
 
 app.listen(PORT, () => console.log("Server listening on port 8080"));
